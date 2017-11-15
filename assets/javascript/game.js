9a00e216698e321112a8b1db602d7edd4299f25f
Bahadur Ghataorhe <b.ghataorhe@gmail.com>
2017-11-14 18:41:21 -0800

    tries to get 1) crystals to have same values when generated 2) total to add

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 //Global variables
 	var numberWins = 0;
 	var numberLosses = 0;
-	var total = 0;
+	// var total = 0;
 	var isClicked = false;
 	var userTotal = $('#userTotal')
 
@@ -16,54 +16,81 @@ $(document).ready(function(){
 	/*Problem is I don't know how to make each image the
 	SAME value each time you click...*/
 
+	//Crystals
+	// $('.gemImage').on("click", function(){
+	// 	isClicked = true;
+	// 	var randomBlue = Math.floor(Math.random() * 12) + 1;
+	// 		$('#userTotal').html(randomBlue + randomGreen + randomRed + randomYellow);
+	// 	var randomGreen = Math.floor(Math.random() * 12) + 1;
+	// 		$('#userTotal').html(randomBlue + randomGreen + randomRed + randomYellow);
+	// 	var randomRed = Math.floor(Math.random() * 12) + 1;
+	// 		$('#userTotal').html(randomBlue + randomGreen + randomRed + randomYellow);
+	// 	var randomYellow = Math.floor(Math.random() * 12) + 1;
+	// 		$('#userTotal').html(randomBlue + randomGreen + randomRed + randomYellow);
+
+	// })
+
+	function randomCrystal(){
+		return Math.floor(Math.random()*12) + 1;
+	}
+
+	//Crystals
+	$('.gemImage').on("click", function(){
+		isClicked = true;
+
+	});
+
 	//Blue
 	$('#crystalOne').on("click", function(){
- 		isClicked = true;
-		var randomBlue = Math.floor(Math.random() * 12) + 1;
-			$('#userTotal').html(randomBlue);
-	})
+  		isClicked = true;
+  		var blue = randomCrystal();
+	 		$('#userTotal').html(blue);
+	});
+	 
 	//Green
 	$('#crystalTwo').on("click", function(){
- 		isClicked = true;
-		var randomGreen = Math.floor(Math.random() * 12) + 1;
-			$('#userTotal').html(randomGreen);
-	})	
+  		isClicked = true;
+  		var green = randomCrystal();
+	 		$('#userTotal').html(green);
+	});	
+	 
 	//Red
 	$('#crystalThree').on("click", function(){
- 		isClicked = true;
-		var randomRed = Math.floor(Math.random() * 12) + 1;
-			$('#userTotal').html(randomRed);
-	})
+  		isClicked = true;
+	 	var red = randomCrystal();
+	 		$('#userTotal').html(red);
+	});
+	
 	//Yellow
 	$('#crystalFour').on("click", function(){
- 		isClicked = true;
-		var randomYellow = Math.floor(Math.random() * 12) + 1;
-			$('#userTotal').html(randomYellow);
-	})
+  		isClicked = true;
+  		var yellow = randomCrystal();
+	 		$('#userTotal').html(yellow);
+	});
 
-//User Total/Results
-	// switch (userTotal) {
-	// 	case ''
-	// }
-	for (var i = 0; i < Things.length; i++) {
-		Things[i]
-	}
-	switch (userTotal) {
-		case '':
-			total = randomYellow + randomRed + randomGreen + randomBlue;
-			$('#userTotal').append(total);
-		break;
-	}
+//User Total/Results	
+	$(".gemImage").on("click", function(){
+	 	switch (userTotal) {
+	 		case 'plus':
+	 			var total = randomYellow + randomRed + randomGreen + randomBlue;
+	 			$('#userTotal').append(total);
+	 		break;	
+		}
+	});
 
+	console.log(userTotal)
+
+//Alerts if Win/Lose
 	if (userTotal == randomNumber) {
+		console.log(userTotal = randomNumber);
 		alert("You win!");
 		$('#numberWins').html(numberWins++);
+
 	} else {
 		alert("Sorry, you lose.");
 		$('#numberLosses').html(numberLosses++);
 	}
-	
 
-})
+});
 
 console.log()
